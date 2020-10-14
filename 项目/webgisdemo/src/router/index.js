9ad1a34f0781ae3de87685866a71e606549503d6
ZhuPengFei3857 <zhupengfei6623@navinfo.com>
2020-10-14 15:56:12 +0800

    fix: 优化路由组件的加载方式

--- 项目/webgisdemo/src/router/index.js
+++ 项目/webgisdemo/src/router/index.js
@@ -1,6 +1,13 @@
+/*
+ * @Author: zhupengfei6623
+ * @Date: 2020-10-14 15:27:28
+ * @Description: file content
+ */
+
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// 如果用import引入的话，当项目打包时路由里的所有component都会打包在一个js中，造成进入首页时，需要加载的内容过多，时间相对比较长。
+// import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -9,7 +16,8 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      // 当你用nodeJS中的require这种方式引入的时候，会将你的component分别打包成不同的js，加载的时候也是按需加载，只用访问这个路由网址时才会加载这个js。
+      component: resolve => require(['../components/HelloWorld.vue'], resolve)
     }
   ]
 })
