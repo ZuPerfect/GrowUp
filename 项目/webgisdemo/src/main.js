9bbd8a8378676b5a94d6ab1d6d307d869e0410bb
ZhuPengFei3857 <zhupengfei6623@navinfo.com>
2020-10-14 16:21:32 +0800

    fix: 安装elementUI

--- 项目/webgisdemo/src/main.js
+++ 项目/webgisdemo/src/main.js
@@ -1,11 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/*
+ * @Author: zhupengfei6623
+ * @Date: 2020-10-14 15:27:28
+ * @Description: file content
+ */
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- 项目/webgisdemo/src/router/index.js
+++ 项目/webgisdemo/src/router/index.js
@@ -7,7 +7,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // 如果用import引入的话，当项目打包时路由里的所有component都会打包在一个js中，造成进入首页时，需要加载的内容过多，时间相对比较长。
-// import HelloWorld from '@/components/HelloWorld'
+// import MainPage from '@/components/MainPage'
 
 Vue.use(Router)
 
@@ -17,7 +17,7 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       // 当你用nodeJS中的require这种方式引入的时候，会将你的component分别打包成不同的js，加载的时候也是按需加载，只用访问这个路由网址时才会加载这个js。
-      component: resolve => require(['../components/HelloWorld.vue'], resolve)
+      component: resolve => require(['../components/MainPage.vue'], resolve)
     }
   ]
 })
