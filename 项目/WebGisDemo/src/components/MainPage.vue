7d268bc96c1ab9d94aa24fe72de301213bca44f6
ZhuPengFei3857 <zhupengfei6623@navinfo.com>
2020-10-15 18:04:17 +0800

    fix: 增加如何定位多边形自相交的错误交点处的解决方案

--- 项目/WebGisDemo/src/components/MainPage.vue
+++ 项目/WebGisDemo/src/components/MainPage.vue
@@ -14,16 +14,37 @@ export default {
   name: 'MainPage',
   data() {
     return {
-      msg: '欢迎来到你的VUE'
     }
   },
   mounted() {
-    var map = L.map('mapCon').setView([39.9, 116.4], 9);
+    var map = L.map('mapCon').setView([39.971495, 116.25144], 12);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '朱鹏飞的地图'
+      attribution: ''
     }).addTo(map);
 
-    L.marker([39.9, 116.4]).addTo(map).bindPopup('北京啊').openPopup();
+    // L.marker([39.91036, 116.403704]).addTo(map).bindPopup('北京啊').openPopup();
+    var polygonLayer = L.geoJSON(null, {
+      style: feature => ({ color: '#f00' })
+    }).addTo(map);
+    var polygonData = {
+      type: "Feature",
+      geometry: {
+        type: "Polygon",
+        coordinates: [[
+          [116.2535, 40.009898], [116.25144, 39.971495], [116.324225, 39.990436],
+          [116.296072, 40.032509], [116.2950, 39.95798], [116.2535, 40.009898]
+        ]]
+      }
+    };
+    polygonLayer.addData(polygonData);
+
+
+
+    /*核心：利用turf里面的kinks方法，计算出所有自相交的点，然后渲染出来*/
+    var kinks = turf.kinks(turf.polygon(polygonData.geometry.coordinates));
+    kinks.features.forEach(kink => {
+      L.marker(kink.geometry.coordinates.reverse()).addTo(map);
+    });
   }
 }
 </script>
