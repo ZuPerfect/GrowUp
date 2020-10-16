390e9f687dbe78e1ed38af890c4f7c74259ac77c
ZhuPengFei3857 <zhupengfei6623@navinfo.com>
2020-10-16 10:56:04 +0800

    fix: 完善注释

--- 项目/WebGisDemo/src/components/MainPage.vue
+++ 项目/WebGisDemo/src/components/MainPage.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: zhupengfei6623
  * @Date: 2020-10-14 15:27:28
- * @Description: file content
+ * @Description: 多边形自相交交点定位
 -->
 <template>
   <div class="main-page">
@@ -17,39 +17,39 @@ export default {
     }
   },
   mounted() {
+    // 创建一个地图容器
     var map = L.map('mapCon').setView([39.971495, 116.25144], 12);
+    // 创建一个瓦片图层并添加到地图容器中
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: ''
     }).addTo(map);
-
-    // L.marker([39.91036, 116.403704]).addTo(map).bindPopup('北京啊').openPopup();
+    // 创建一个多边形矢量图层
     var polygonLayer = L.geoJSON(null, {
       style: feature => ({ color: '#f00' })
     }).addTo(map);
+    // 创建一个多边形geoJSON
     var polygonData = {
       type: "Feature",
       geometry: {
         type: "Polygon",
         coordinates: [[
           [116.2535, 40.009898], [116.25144, 39.971495], [116.324225, 39.990436],
-          [116.296072, 40.032509], [116.2950, 39.95798], [116.2535, 40.009898]
+          [116.296072, 40.032509], [116.2950, 39.94798], [116.2535, 40.009898]
         ]]
       }
     };
+    // 将geoJSON添加到图层中
     polygonLayer.addData(polygonData);
-
-
 
     /*核心：利用turf里面的kinks方法，计算出所有自相交的点，然后渲染出来*/
     var kinks = turf.kinks(turf.polygon(polygonData.geometry.coordinates));
     kinks.features.forEach(kink => {
-      L.marker(kink.geometry.coordinates.reverse()).addTo(map);
+      L.marker(kink.geometry.coordinates.reverse()).addTo(map).bindPopup('这是一个交点');
     });
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .main-page {
   width: 100%;
